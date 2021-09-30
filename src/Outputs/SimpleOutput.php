be22ec1e3f6d8d34f35797b4020283871d1d5896
Khalid Umar <khalidumar03@gmail.com>
2021-10-01 03:16:10 +0400

    output implementation added

--- src/Outputs/SimpleOutput.php
+++ src/Outputs/SimpleOutput.php
@@ -6,10 +6,19 @@ use CronParser\Cron;
 
 class SimpleOutput implements OutputInterface
 {
+    const SPACES = 15;
+
     public function display(Cron $cron): void
     {
         foreach ($cron->getFields() as $field) {
-            echo $field->getName() . " " . print_r($field->getNextRuns()) . " " . $cron->getCommand();
+            echo $field->getName() . $this->makeSpace($field->getName()) . implode(" ", $field->getNextRuns()) . "\n";
         }
+
+        echo "command" . $this->makeSpace("command") . $cron->getCommand() . "\n";
+    }
+
+    private function makeSpace($field) : string
+    {
+        return str_repeat(" ", self::SPACES - strlen($field));
     }
 }
